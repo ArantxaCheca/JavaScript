a0f90d41944bc7774404767b14f33d90f31f0f05
Arantxa C <checaarantxa6@gmail.com>
2026-01-20 13:12:13 +0100

    Exercises

---
+++ EXERCISES/ex1.js
@@ -0,0 +1,22 @@
+const saludo = 'Hello World';
+console.log(saludo);
+
+const nombre = 'Arantxa';
+console.log(nombre);
+
+let edad = 22;
+console.log(edad);
+edad = 23;
+console.log(edad);
+
+let num1 = 5;
+let num2 = 7;
+let resultado = num1 + num2;
+console.log(resultado);
+
+let ob1 = 'Soy';
+let ob2 = 'ArantxaC';
+let conc = ob1 + ' ' + ob2;
+console.log(conc);
+
+
